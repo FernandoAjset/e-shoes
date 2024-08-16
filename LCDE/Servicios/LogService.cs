87b5645d5f4c72a871cefe5b95016771be66477b
Ferajset-G3 <fernando_ajset@hotmail.com>
2024-08-16 08:26:24 -0600

    Implementación bitacora de logs errores.

---
+++ LCDE/Servicios/LogService.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace LCDE.Servicios
+{
+    public interface ILogService
+    {
+        void LogError(Exception ex);
+    }
+
+    public class LogService : ILogService
+    {
+        private readonly IConfiguration configuration;
+        private readonly string connectionString;
+
+        public LogService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            connectionString = configuration.GetConnectionString("ConnectionLCDE");
+        }
+
+        public void LogError(Exception ex)
+        {
+            // guardar error en base de datos usando dapper.
+            using var conexion = new SqlConnection(connectionString);
+            conexion.Execute("INSERT INTO LogError (Message, StackTrace, Date) VALUES (@Message, @StackTrace, @Date)", new
+            {
+                ex.Message,
+                ex.StackTrace,
+                Date = DateTime.Now
+            });
+        }
+    }
+}
