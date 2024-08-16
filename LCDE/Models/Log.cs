e288f148ddaed3bd0e6539193478943bc57b5df8
Ferajset-G3 <fernando_ajset@hotmail.com>
2024-08-16 10:01:04 -0600

    Agregado log para info de envio correo.

--- LCDE/Models/Log.cs
+++ LCDE/Models/Log.cs
@@ -1,8 +1,9 @@
 ﻿namespace LCDE.Models
 {
-    public class LogError
+    public class Log
     {
         public int Id { get; set; }
+        public string Type { get; set; }
         public string Message { get; set; }
         public string StackTrace { get; set; }
         public DateTime Date { get; set; }
--- LCDE/Servicios/LogService.cs
+++ LCDE/Servicios/LogService.cs
@@ -1,11 +1,12 @@
 ﻿using Dapper;
+using LCDE.Models;
 using Microsoft.Data.SqlClient;
 
 namespace LCDE.Servicios
 {
     public interface ILogService
     {
-        void LogError(Exception ex);
+        void Log(Log log);
     }
 
     public class LogService : ILogService
@@ -19,15 +20,16 @@ namespace LCDE.Servicios
             connectionString = configuration.GetConnectionString("ConnectionLCDE");
         }
 
-        public void LogError(Exception ex)
+        public void Log(Log log)
         {
             // guardar error en base de datos usando dapper.
             using var conexion = new SqlConnection(connectionString);
-            conexion.Execute("INSERT INTO LogError (Message, StackTrace, Date) VALUES (@Message, @StackTrace, @Date)", new
+            conexion.Execute("INSERT INTO Log (Type, Message, StackTrace, Date) VALUES (@Type, @Message, @StackTrace, @Date)", new
             {
-                ex.Message,
-                ex.StackTrace,
-                Date = DateTime.Now
+                log.Type,
+                log.Message,
+                log.StackTrace,
+                log.Date
             });
         }
     }
