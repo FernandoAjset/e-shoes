fbae68136a61957d6236dd3a071ec5bcb4022006
Ferajset-G3 <fernando_ajset@hotmail.com>
2024-08-17 08:50:30 -0600

    Agregado validaciones a modelos de crear cliente.

--- LCDE/Models/ClienteDTO.cs
+++ LCDE/Models/ClienteDTO.cs
@@ -11,8 +11,10 @@ namespace LCDE.Models
     public class DatosCliente
     {
         [Required(ErrorMessage = "El campo {0} es requerido, puede colocar CF")]
+        [RegularExpression(@"^(?!0+$)[a-zA-Z0-9-]+$", ErrorMessage = "El campo solo puede contener letras, números y guiones, y no puede ser solo ceros")]
         public string NIT { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Solo se permiten letras.")]
         public string Nombre { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")]
         public string Direccion { get; set; }
@@ -20,6 +22,7 @@ namespace LCDE.Models
         [DataType(DataType.PhoneNumber, ErrorMessage = "El formato del telefono no es válido")]
         [MinLength(8, ErrorMessage = "El campo debe ser de 8 caracteres")]
         [MaxLength(8, ErrorMessage = "El campo debe ser de 8 caracteres")]
+        [RegularExpression(@"^(?!0+$)\d{8}$", ErrorMessage = "El campo no puede contener solo ceros")]
         public string? Telefono { get; set; }
     }
 
@@ -27,6 +30,7 @@ namespace LCDE.Models
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Solo se permiten letras.")]
         public string Nombre_usuario { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [EmailAddress(ErrorMessage = "El campo debe ser un correo electrónico válido")]
--- LCDE/Models/CrearClienteDTO.cs
+++ LCDE/Models/CrearClienteDTO.cs
@@ -5,19 +5,23 @@ namespace LCDE.Models
 {
     public class CrearClienteDTo
     {
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Solo se permiten letras.")]
+        public string Nombre { get; set; }
 
         [Required(ErrorMessage = "El campo {0} es requerido")]
-        public string Nombre { get; set; }
-        [Required(ErrorMessage = "El campo {0} es requerido")]
         public string Direccion { get; set; }
+
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [DataType(DataType.PhoneNumber, ErrorMessage = "El formato del telefono no es válido")]
         [MinLength(8, ErrorMessage = "El campo debe ser de 8 caracteres")]
         [MaxLength(8, ErrorMessage = "El campo debe ser de 8 caracteres")]
+        [RegularExpression(@"^(?!0+$)\d{8}$", ErrorMessage = "El campo no puede contener solo ceros")]
         public string? Telefono { get; set; }
-        [Required(ErrorMessage = "El campo {0} es requerido, puede colocar CF")]
-        public string? Nit { get; set; }
 
+        [Required(ErrorMessage = "El campo {0} es requerido, puede colocar CF")]
+        [RegularExpression(@"^(?!0+$)[a-zA-Z0-9-]+$", ErrorMessage = "El campo solo puede contener letras, números y guiones, y no puede ser solo ceros")]
+        public string? Nit { get; set; }
     }
 
     public class CrearUsarioCliente
@@ -29,20 +33,22 @@ namespace LCDE.Models
     public class RegistroUsuarioCliente
     {
         public int? Id { get; set; }
+
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [MaxLength(70, ErrorMessage = "El campo no puede exceder 70 caracteres")]
         [Display(Name = "Nombre de usuario")]
         [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Solo se permiten letras.")]
         public string Nombre_usuario { get; set; }
+
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [EmailAddress(ErrorMessage = "El campo debe ser un correo electrónico válido")]
         [Display(Name = "Correo electrónico")]
         public string Correo { get; set; }
+
         [DataType(DataType.Password)]
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [MinLength(8, ErrorMessage = "El campo debe tener al menos 8 caracteres")]
         [Display(Name = "Contraseña")]
         public string Contrasennia { get; set; }
-
     }
 }
