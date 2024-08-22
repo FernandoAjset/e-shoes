0ec4bbc2be7d735b381ec992ed20f9bb1773b680
Kat <jirehrecinos@gmail.com>
2024-08-22 10:05:09 -0600

    Se agregó la lista de productos en búsqueda por categoria

--- LCDE/Controllers/EcommerceProductoController.cs
+++ LCDE/Controllers/EcommerceProductoController.cs
@@ -54,37 +54,32 @@ namespace LCDE.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> BuscarPorCategorias()
+        public async Task<IActionResult> BuscarPorCategorias(int CategoriaId)
         {
             ProductoFiltroDTO obj = new ProductoFiltroDTO();
             obj.Categorias = (await repositorioCategorias.ObtenerTodosCategorias()).ToList();
+            obj.CategoriaId = CategoriaId;
             obj.PrecioMin = 0;
             obj.PrecioMax = await repositorioProductos.PrecioMaximo();
+            obj.NombreProducto = "";
+            obj.productosListarDTO = await repositorioProductos.ObtenerProductoFiltrado(obj);
             return View(obj);
         }
 
         [HttpPost]
         public async Task<IActionResult> BuscarPorCategorias(ProductoFiltroDTO productoFiltrar)
-        { 
+        {
             try
             {
-                if (!ModelState.IsValid)
-                {
-                    return View(productoFiltrar);
-                }
-               
-                List<ProductoListarDTO> filtro = await repositorioProductos.ObtenerProductoFiltrado(productoFiltrar);
+                List<ProductoListarDTO> productos = await repositorioProductos.ObtenerProductoFiltrado(productoFiltrar);
+                productoFiltrar.productosListarDTO = productos;
 
-                // Validación de que se encontró alguna categoría
-                if (filtro == null || !filtro.Any())
-                {
-                    // Si no se encontró la categoría, redirigir a la acción "NoEncontrado"
-                    return RedirectToAction("NoEncontrado", "Home");
-                }
-                return View(filtro);//Aqui tengo una duda, no sé cual es el tipo de dato que lleva la vista que se va a retornar
+                return StatusCode(200, productoFiltrar);
             }
-            catch (Exception ex) { return RedirectToAction("Error", "Home"); }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Error", "Home");
+            }
         }
     }
-
 }
--- LCDE/Models/ProductoFiltroDTO.cs
+++ LCDE/Models/ProductoFiltroDTO.cs
@@ -4,12 +4,15 @@
     {
         public int CategoriaId { get; set; }
 
-        public string NombreProducto { get; set; }
+        public string? NombreProducto { get; set; }
 
         public decimal PrecioMin { get; set; }
 
         public decimal PrecioMax { get; set; }
 
-        public List<Categoria> Categorias { get; set; }
+        public List<Categoria>? Categorias { get; set; }
+        public List<ProductoListarDTO>? productosListarDTO { get; set; }
+
+        // otra propiedad de tipo lista productos, puede ser lista vacia no nula, de tipo ProductoListarDTO
     }
 }
