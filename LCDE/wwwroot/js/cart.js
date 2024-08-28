3fa72028cf9f6fa5f7825910a6f087b29719ca57
eajset <eajset@lucalza.com>
2024-08-28 11:36:36 -0600

    Avance agregar producto a carrito desde busqueda por categoria.

---
+++ LCDE/wwwroot/js/cart.js
@@ -0,0 +1,50 @@
+﻿function showToast(type, message, duration = 1500) {
+    let toastElement, toastBodyElement;
+
+    if (type === 'success') {
+        toastElement = document.getElementById('success-toast');
+        toastBodyElement = document.getElementById('success-toast-body');
+    } else if (type === 'error') {
+        toastElement = document.getElementById('error-toast');
+        toastBodyElement = document.getElementById('error-toast-body');
+    }
+
+    if (toastElement && toastBodyElement) {
+        toastBodyElement.textContent = message;
+        let toast = new bootstrap.Toast(toastElement, { delay: duration });
+        toast.show();
+    }
+}
+
+function addToCart(productId, quantity) {
+    quantity = parseInt(quantity, 10);
+
+    if (isNaN(quantity) || quantity <= 0) {
+        showToast('error', 'Por favor, ingrese una cantidad válida.');
+        return;
+    }
+
+    let cart = JSON.parse(localStorage.getItem('shoppingCart')) || [];
+    let productIndex = cart.findIndex(item => item.id === productId);
+
+    if (productIndex !== -1) {
+        let existingQuantity = parseInt(cart[productIndex].quantity, 10);
+        if (isNaN(existingQuantity)) {
+            existingQuantity = 0;
+        }
+        cart[productIndex].quantity = existingQuantity + quantity;
+    } else {
+        cart.push({ id: productId, quantity: quantity });
+    }
+
+    localStorage.setItem('shoppingCart', JSON.stringify(cart));
+
+    // Disparar evento de almacenamiento local
+    window.dispatchEvent(new Event('storage'));
+
+    // Mostrar el toast de éxito
+    showToast('success', 'Producto agregado al carrito.');
+
+    // Actualizar el contador del carrito
+    updateCartCount();
+}
